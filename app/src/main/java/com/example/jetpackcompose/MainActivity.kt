5513fdf6d81b9a39475966331a8cf32b6eca7e8d
Milo <milo@Milos-MacBook-Pro.local>
2022-06-06 07:46:02 -0500

    add-new-name-without-recomposition

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -3,12 +3,18 @@ package com.example.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
-val namesList: List<String> = listOf(
+val namesList: ArrayList<String> = arrayListOf(
     "John",
     "Michael",
     "Andrew",
@@ -27,16 +33,27 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingList(names: List<String>) {
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         for (name in names) {
             Greeting(name = name)
+        }
+
+        Button(onClick = { namesList.add("Milo") }) {
+            Text("Add new name")
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(
+        text = "Hello $name!",
+        style = MaterialTheme.typography.h4
+    )
 }
 
 @Preview(showBackground = true)
