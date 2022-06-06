903b17a7ec959b5ca93bf46734031b87089bc53f
Milo <milo@Milos-MacBook-Pro.local>
2022-06-05 19:47:12 -0500

    Adding surface to screen composable

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -19,28 +22,21 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GreetingText(name = "world")
+            MainScreen()
         }
     }
-
 }
 
 @Composable
-fun GreetingText(name: String) {
-    Text(
-        text = " Hello $name!",
-        modifier = Modifier
-            .clickable(onClick = { })
-            .padding(all = 24.dp)
-            .width(200.dp)
-            .height(240.dp),
-        style = MaterialTheme.typography.h5,
-        fontWeight = FontWeight.SemiBold
-    )
+fun MainScreen() {
+    Surface(color = Color.DarkGray,
+        modifier = Modifier.fillMaxSize()) {
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreviewMainActivity() {
-    GreetingText(name = "world")
+fun DefaultPreview() {
+    MainScreen()
 }
