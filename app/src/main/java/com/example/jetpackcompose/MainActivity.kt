62ef7908e0bf42fe71d302c249493a51d0f442c5
Milo <milo@Milos-MacBook-Pro.local>
2022-06-05 20:01:20 -0500

    basic-layout-row-solution

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -3,18 +3,14 @@ package com.example.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -29,9 +25,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
-    Surface(color = Color.DarkGray,
-        modifier = Modifier.fillMaxSize()) {
-
+    Surface(
+        color = Color.DarkGray,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Row {
+            Surface(
+                color = Color.Magenta,
+                modifier = Modifier
+                    .height(600.dp)
+                    .width(60.dp)
+            ) { }
+            Surface(
+                color = Color.Red,
+                modifier = Modifier
+                    .height(600.dp)
+                    .width(60.dp)
+            ) { }
+        }
     }
 }
 
