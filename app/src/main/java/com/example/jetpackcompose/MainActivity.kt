0a47cc53b06a56bd941448cb50399cd4a84a0ae9
Milo <milo@Milos-MacBook-Pro.local>
2022-06-05 19:29:00 -0500

    click-effect-padding-solution

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -3,48 +3,40 @@ package com.example.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.jetpackcompose.ui.theme.JetPackComposeTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetPackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    GreetingButton()
-                }
-            }
+            GreetingText(name = "world")
         }
     }
+
 }
 
 @Composable
 fun GreetingText(name: String) {
-    Text(text = " Hello $name!")
-}
-
-@Composable
-fun GreetingButton() {
-    Button(onClick = { }) {
-        GreetingText(name = "button")
-        GreetingText(name = "remaining button")
-    }
+    Text(
+        text = " Hello $name!",
+        modifier = Modifier
+            .width(200.dp)
+            .height(240.dp)
+            .clickable(onClick = { })
+            .padding(all = 24.dp)
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreviewMainActivity() {
-    GreetingButton()
+    GreetingText(name = "world")
 }
