cf9d57e5ff0b2e4990659a7a96251d3986b81691
Milo <milo@Milos-MacBook-Pro.local>
2022-06-06 07:40:15 -0500

    dynamic-content-example

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -3,67 +3,44 @@ package com.example.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+
+val namesList: List<String> = listOf(
+    "John",
+    "Michael",
+    "Andrew",
+    "Danna",
+    "Georgia"
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainScreen()
+            GreetingList(names = namesList)
         }
     }
 }
 
 @Composable
-fun MainScreen() {
-    Surface(
-        color = Color.DarkGray,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.SpaceEvenly,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly,
-            ) {
-                ColoredSquare(Color.Red)
-                ColoredSquare(Color.Magenta)
-            }
-            ColoredSquare(Color.Cyan)
-            ColoredSquare(Color.Yellow)
-            ColoredSquare(Color.Blue)
+fun GreetingList(names: List<String>) {
+    Column {
+        for (name in names) {
+            Greeting(name = name)
         }
     }
 }
 
 @Composable
-fun ColoredSquare(color: Color) {
-    Surface(
-        color = color,
-        modifier = Modifier
-            .height(100.dp)
-            .width(100.dp)
-    ) { }
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MainScreen()
+    GreetingList(names = namesList)
 }
